ddd09567e48e0b6679ef462326cbd540696cba68
André Loureiro <contato.loureiro1@gmail.com>
2024-12-31 11:25:44 -0300

    feat: creating modal component

--- app/src/Main/index.tsx
+++ app/src/Main/index.tsx
@@ -10,8 +10,18 @@ import {
 import { Categories } from "../components/Categories";
 import { Button } from "../components/Button";
 import { Menu } from "../components/Menu";
+import { TableModal } from "../components/TableModal";
 
 export function Main() {
+  const [isTableModalVisible, setIsTableModalVisible] = React.useState(false);
+  const [selectedTable, setSelectedTable] = React.useState("");
+
+  function handleSaveTable(table: string) {
+    console.log(table);
+    setSelectedTable(table);
+    setIsTableModalVisible(false);
+  }
+
   return (
     <>
       <Container>
@@ -21,15 +31,25 @@ export function Main() {
           <Categories />
         </CategoriesContainer>
 
-        <MenuContainer >
+        <MenuContainer>
           <Menu />
         </MenuContainer>
       </Container>
       <Footer>
         <FooterConteiner>
-          <Button>Novo Pedido</Button>
+          {!selectedTable && (
+            <Button onPress={() => setIsTableModalVisible(true)}>
+              Novo Pedido
+            </Button>
+          )}
         </FooterConteiner>
       </Footer>
+
+      <TableModal
+        visable={isTableModalVisible}
+        onClose={() => setIsTableModalVisible(false)}
+        onSave={handleSaveTable}
+      />
     </>
   );
 }
