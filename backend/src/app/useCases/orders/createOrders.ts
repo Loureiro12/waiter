f55a7fdad3d8f643e07332cf206259462fe93448
André Loureiro <contato.loureiro1@gmail.com>
2025-01-06 14:02:42 -0300

    feat: implement socket

--- backend/src/app/useCases/orders/createOrders.ts
+++ backend/src/app/useCases/orders/createOrders.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { Order } from "../../models/Order";
+import { io } from "../../..";
 
 export async function createOrders(req: Request, res: Response) {
   try {
@@ -9,6 +10,9 @@ export async function createOrders(req: Request, res: Response) {
       table,
       products,
     });
+
+    const orderDetails = await order.populate('products.product');
+    io.emit("orders@new", orderDetails);
 
     res.status(201).json(order);
   } catch {
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,30 @@
 import path from "node:path";
+import http from "node:http";
+import {Server} from 'socket.io'
 
 import express from "express";
 import mongoose from "mongoose";
 import { router } from "./app/router";
 
+const app = express();
+const server = http.createServer(app);
+export const io = new Server(server);
+
+
 mongoose
   .connect("mongodb://localhost:27017/test")
   .then(() => {
-    const app = express();
     const port = 3001;
 
+    app.use((req, res, next) => {
+      res.setHeader("Access-Control-Allow-Origin", "*");
+      res.setHeader("Access-Control-Allow-Methods", "*");
+      res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Content-Type, Authorization"
+      );
+      next();
+    });
     app.use(
       "/uploads",
       express.static(path.resolve(__dirname, "..", "uploads"))
@@ -17,7 +32,7 @@ mongoose
     app.use(express.json());
     app.use(router);
 
-    app.listen(3001, () => {
+    server.listen(3001, () => {
       console.log(`Server is running on port ${port}`);
     });
   })
